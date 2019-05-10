26debbb948cf7727682e43a3e2076a2572df4cce
Martina <martina.gabrielli@yahoo.it>
2019-05-10 18:48:21 +0100

    Added functions etc

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+var finished = false;
+var count = 0;
+
 function moveTurtle(){
+    if(finished === true){
+        defaultTurtle();
+        setTimeout(function(){
+            jQuery('#turtle').removeClass('flip');
+        },1000);
+        finished = false;
+    } else {
+        finished = true;
+    jQuery('.button').addClass('default');
     jQuery('#turtle').addClass('animate');
+
+  
+
+    }
+    
 }
 
-jQuery('.start').click(moveTurtle);
+function defaultTurtle(){
+    jQuery('#turtle').addClass('flip');
+    jQuery('#turtle').removeClass('animate');
+    jQuery('.button').removeClass('default');
+    jQuery('.button').addClass('start');
+    
+}
+
+jQuery('.button').click(moveTurtle);
